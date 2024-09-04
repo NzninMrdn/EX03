967d0d2d1cded1186a29c3c59605a13c2aba9348
Nazanin <nazanin16mrdn@gmail.com>
2024-09-04 22:46:44 +0330

    tamrin 2

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,16 @@
 // write your func here
+function isPalindrome(str) {
+    // Invert string to lowercase letters and delete spaces
+    const cleanedStr = str.toLowerCase().replace(/\s+/g, '');
+    
+    const reversedStr = cleanedStr.split('').reverse().join('');
+    
+    return cleanedStr === reversedStr;
+}
+
+const input = "racecar";
+if (isPalindrome(input)) {
+    console.log(true);
+} else {
+   console.log(false);
+}

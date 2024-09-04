a66eba01faecf342d791f5570a5b0fa52dff7aae
Nazanin <nazanin16mrdn@gmail.com>
2024-09-04 23:02:26 +0330

    tamrin 3

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,12 @@
 // write your func here
+function factorial(n) {
+    if (n < 0) {
+        return "Does not exist.";
+    } else if(n === 1 || n === 0){
+        return 1;
+    } else {
+        return n * factorial(n-1);
+    }
+}
+
+console.log(factorial(3));

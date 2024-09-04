99fe0b58027a8e28a5da0cc36f55ce7d5a44b03b
Nazanin <nazanin16mrdn@gmail.com>
2024-09-04 22:37:40 +0330

    tamrin 1

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,9 @@
 // write your func here
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+const input = "this is a string";
+const output = reverseString(input);
+
+console.log(output);

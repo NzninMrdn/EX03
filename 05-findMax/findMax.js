cee1b966ca9380f49e0dc7c4eeb2701660e955b1
Nazanin <nazanin16mrdn@gmail.com>
2024-09-08 11:27:36 +0330

    tamrin 5

--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,21 @@
 // write your func here
+function findMax(array) {
+
+    if (array.length === 0) {
+        return undefined; 
+    }
+
+    let max = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i]; 
+        }
+    }
+
+    return max; 
+}
+
+const numbers = [3, 5, 7, 2, 8, -10, 4];
+const largestNumber = findMax(numbers);
+console.log(largestNumber); 
